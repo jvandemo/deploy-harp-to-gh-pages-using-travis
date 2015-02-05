ad0a64ab0df2dbced6f16e02137015b9f9eebb6b
Travis-CI <travis@jvandemo.com>
2015-02-05 15:05:01 +0100

    feat(deploy): put temp directory name in variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,14 +2,15 @@
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
+TEMP_DIRECTORY="/tmp/__temp_static_content"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GH_TOKEN@github.com}
 
 echo "Compiling new static content"
-mkdir /tmp/__temp_static_content || exit 1
-harp compile . /tmp/__temp_static_content || exit 1
-cp .gitignore /tmp/__temp_static_content || exit 1
+mkdir $TEMP_DIRECTORY || exit 1
+harp compile . $TEMP_DIRECTORY || exit 1
+cp .gitignore $TEMP_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
 git checkout -B gh-pages || exit 1
@@ -18,8 +19,8 @@ echo "Removing old static content"
 git rm -rf . || exit 1
 
 echo "Copying newly generated static content"
-cp -r /tmp/__temp_static_content/* . || exit 1
-cp /tmp/__temp_static_content/.gitignore . || exit 1
+cp -r $TEMP_DIRECTORY/* . || exit 1
+cp $TEMP_DIRECTORY/.gitignore . || exit 1
 
 echo "Pushing new content to $ORIGIN_URL"
 git config user.name "Travis-CI" || exit 1
@@ -30,7 +31,7 @@ git commit --allow-empty -m "Regenerated static content for $CURRENT_COMMIT" || 
 git push --force --quiet "$ORIGIN_URL_WITH_CREDENTIALS" gh-pages > /dev/null 2>&1
 
 echo "Cleaning up temp files"
-rm -Rf /tmp/__temp_static_content
+rm -Rf $TEMP_DIRECTORY
 
 echo "Deployed successfully."
 exit 0
